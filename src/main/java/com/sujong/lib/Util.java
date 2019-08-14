78f490500cec4989dccf13b5aab7cb1e560fd2b3
sujong <chaesujong@hanbitsoft.co.kr>
2019-08-14 11:17:02 +0900

    test2

--- src/main/java/com/sujong/lib/Util.java
+++ src/main/java/com/sujong/lib/Util.java
@@ -6,4 +6,12 @@ public class Util {
     public static void test(){
         System.out.println("s");
     }
+
+    public static void test2(){
+
+        int a = 1234;
+        int b = 2345;
+
+        a = a + b;
+    }
 }
